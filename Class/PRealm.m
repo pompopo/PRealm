db03bfcd758eb3f536ec495fec7f158ba149f99c
pompopo <pompopo@pompopo-no-MacBook-Pro.local>
2015-05-31 23:38:23 +0900

    fix indent

--- Class/PRealm.m
+++ Class/PRealm.m
@@ -9,64 +9,63 @@
 
 @implementation PRealm
 RCT_EXPORT_MODULE();
+
 RCT_EXPORT_METHOD(registerScheme:(NSString *)name scheme:(NSDictionary *)dict) {
-  [PRealmObject createClassNamed:name scheme:dict];
+    [PRealmObject createClassNamed:name scheme:dict];
 }
 
 RCT_EXPORT_METHOD(add:(NSString *)name obj:(NSDictionary *)dict) {
-  RLMRealm *realm = [RLMRealm defaultRealm];
-  [realm transactionWithBlock:^{
-    id obj = [PRealmObject instanceWithClassNamed:name dictionary:dict];
-    [realm addObject:obj];
-  }];
+    RLMRealm *realm = [RLMRealm defaultRealm];
+    [realm transactionWithBlock:^{
+        id obj = [PRealmObject instanceWithClassNamed:name dictionary:dict];
+        [realm addObject:obj];
+    }];
 }
 
-
 RCT_EXPORT_METHOD(update:(NSString *)name obj:(NSDictionary *)dict) {
-  RLMRealm *realm = [RLMRealm defaultRealm];
+    RLMRealm *realm = [RLMRealm defaultRealm];
     Class klass = NSClassFromString(name);
     if ([klass respondsToSelector:@selector(primaryKey)]) {
-      NSString *primaryKey = [klass performSelector:@selector(primaryKey)];
-      NSString *query = [NSString stringWithFormat:@"%@ == %@", primaryKey, dict[primaryKey]];
-      RLMResults *results = [klass performSelector:@selector(objectsWhere:) withObject:query];
-      RLMObject *obj = results[0];
-      [realm transactionWithBlock:^{
-          NSMutableDictionary *mutableDict = dict.mutableCopy;
-          [mutableDict removeObjectForKey:primaryKey];
-          [obj setValuesForKeysWithDictionary:mutableDict];
+        NSString *primaryKey = [klass performSelector:@selector(primaryKey)];
+        NSString *query = [NSString stringWithFormat:@"%@ == %@", primaryKey, dict[primaryKey]];
+        RLMResults *results = [klass performSelector:@selector(objectsWhere:) withObject:query];
+        RLMObject *obj = results[0];
+        [realm transactionWithBlock:^{
+            NSMutableDictionary *mutableDict = dict.mutableCopy;
+            [mutableDict removeObjectForKey:primaryKey];
+            [obj setValuesForKeysWithDictionary:mutableDict];
 
-          [realm addOrUpdateObject:obj];
-      }];
-
-  } else {
-      [self add:name obj:dict];
-  }
+            [realm addOrUpdateObject:obj];
+        }];
+    } else {
+        [self add:name obj:dict];
+    }
 }
 
 RCT_EXPORT_METHOD(deleteAllObjects) {
-  RLMRealm *realm = [RLMRealm defaultRealm];
-  [realm transactionWithBlock:^{
-    [realm deleteAllObjects];
-  }];
+    RLMRealm *realm = [RLMRealm defaultRealm];
+    [realm transactionWithBlock:^{
+        [realm deleteAllObjects];
+    }];
 }
 
 RCT_EXPORT_METHOD(find:(NSString *)name callback:(RCTResponseSenderBlock)block) {
-  Class klass = NSClassFromString(name);
-  RLMResults *results = [klass performSelector:@selector(allObjects)];
-  returnResults(results, block);
+    Class klass = NSClassFromString(name);
+    RLMResults *results = [klass performSelector:@selector(allObjects)];
+    returnResults(results, block);
 }
 
 RCT_EXPORT_METHOD(find:(NSString *)name where:(NSString *)where callback:(RCTResponseSenderBlock)block) {
-  Class klass = NSClassFromString(name);
-  RLMResults *results = [klass performSelector:@selector(objectsWhere:) withObject:where];
-  returnResults(results, block);
+    Class klass = NSClassFromString(name);
+    RLMResults *results = [klass performSelector:@selector(objectsWhere:) withObject:where];
+    returnResults(results, block);
 }
 
 void returnResults(RLMResults *results, RCTResponseSenderBlock block) {
-  NSMutableArray *array = @[].mutableCopy;
-  for (PRealmObject *obj in results) {
-    [array addObject:[obj dictionary]];
-  }
-  block(@[array]);
+    NSMutableArray *array = @[].mutableCopy;
+    for (PRealmObject *obj in results) {
+        [array addObject:[obj dictionary]];
+    }
+    block(@[array]);
 }
 @end
